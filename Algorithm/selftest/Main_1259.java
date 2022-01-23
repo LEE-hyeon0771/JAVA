c0e169b325a1421fd00f600ded1fd708d9ee8838
LEE-hyeon <84756586+LEE-hyeon0771@users.noreply.github.com>
2022-01-23 10:55:35 +0900

    Update Main_1259.java

--- Algorithm/selftest/Main_1259.java
+++ Algorithm/selftest/Main_1259.java
@@ -1,4 +1,3 @@
-
 package selftest;
 
 import java.io.BufferedReader;
@@ -12,14 +11,14 @@ public class Main_1259 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		while(true) {
-			String str = br.readLine();
+			String str = br.readLine();            //str 입력받기
 			
 			String arr[] = new String[str.length()];        //str을 문자별로 잘라서 담을 배열
-			if(str.equals("0")) {
+			if(str.equals("0")) {                          //0이면 break
 				break;
 			}
 			for(int i = 0; i<str.length(); i++) {          //str을 substring으로 잘라서 하나씩만 arr배열에 담아줌
-				arr[i] = str.substring(i,i+1);
+				arr[i] = str.substring(i,i+1);                 //type은 String이지만 charAt(i)와 동일한 기능
 			}
 			
 			List<String> list = new ArrayList<String>();  //str을 뒤집어서 담을 list
@@ -31,19 +30,19 @@ public class Main_1259 {
 			int count = 0;
 			for(int i = 0; i<str.length(); i++) {
 				
-				String s = list.get(i);
-				if(s.contains(arr[i])) {
+				String s = list.get(i);                 //list에 담긴 string 문자 하나를 불러옴
+				if(s.contains(arr[i])) {                 //s가 arr[i]에 들어있다면 count++
 					count++;
 				}
 			}
 			
-			if(count == str.length()) {
+			if(count == str.length()) {                 //count가 ++되어서 str의 길이와 똑같다면 펠린드롬수임
 				sb.append("yes" + "\n");
 			}
 			else {
 				sb.append("no" + "\n");
 			}	
 		}
-		System.out.println(sb.toString());
+		System.out.println(sb.toString());            //StringBuilder로 담아서 한번에 출력
 	}
 }
