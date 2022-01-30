c1a484c4f462fb1356f882ed4fa656037d511089
LEE-hyeon <84756586+LEE-hyeon0771@users.noreply.github.com>
2022-01-30 17:03:05 +0900

    Update ProgrammersKakao2019_level2_openchat.java

--- Algorithm/selftest/ProgrammersKakao2019_level2_openchat.java
+++ Algorithm/selftest/ProgrammersKakao2019_level2_openchat.java
@@ -1,38 +1,41 @@
 package selftest;
 import java.util.*;
 public class ProgrammersKakao2019_level2_openchat {
-	public String[] solution(String[] record) {
-	        List<String> list = new ArrayList<String>();  //출력 문구 
+	    public String[] solution(String[] record) {
+	        List<String[]> list = new ArrayList<String[]>();  //출력 문구 
 	        Map<String, String> map = new HashMap<String, String>();  //key = uid, value = name
-	        
 	        for(int i = 0; i<record.length; i++){
 	            String[] arr = record[i].split(" ");
 	            if(arr[0].equals("Enter")){
 	                map.put(arr[1],arr[2]);
-	                list.add(arr[1]);      //Enter일 때 uid대입(본질은 그대로)
+	                list.add(arr);      //Enter일 때 uid대입(본질은 그대로)
 	            }
 	            else if(arr[0].equals("Change")){
 	                map.put(arr[1],arr[2]);
 	            }
-	            else{
-	                list.add(arr[1]);      //Leave일 때 uid 대입(본질은 그대로)
-	            }
+	           else{
+	               
+	                list.add(arr);      //Leave일 때 uid 대입(본질은 그대로)
+	           }
 	        }
 	        String[] answer = new String[list.size()];
 	        int index = 0;
-	        for (int i = 0; i<answer.length; i++){
-	            String[] arr = record[i].split(" ");
-	            String nickname = map.get(arr[1]);
+	        for (String[] result : list){
+	            String nickname = map.get(result[1]);
 	            
-	            if (arr[0].equals("Enter")) { 
-	                answer[index++] = nickname + "님이 들어왔습니다.";
+	            if (result[0].equals("Enter")) { 
+	                answer[index] = nickname + "님이 들어왔습니다.";
+	                index++;
 	            } 
-	            if (arr[0].equals("Leave")) {
-	                answer[index++] = nickname + "님이 나갔습니다.";
+	            if(result[0].equals("Leave")) {
+	                answer[index] = nickname + "님이 나갔습니다.";
+	                index++;
 	            }
+	           
 	        }
 	        return answer;
-	   }
-}
+	    }
+	}
+
 
 
